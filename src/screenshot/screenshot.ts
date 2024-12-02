483540b2235d20f629551531a11ef7114e926d08
Sawyer Pangborn <sawyer@anesti.org>
2024-12-02 15:00:43 -0700

    Error handling for screenshot failures
    puppeteer on Linux docker is having some issues, debugging

--- src/screenshot/screenshot.ts
+++ src/screenshot/screenshot.ts
@@ -2,25 +2,31 @@ import puppeteer from 'puppeteer';
 
 
 export async function captureScreenshot(hex:string, url: string): Promise<Uint8Array> {
-    // Create a browser instance
-    const browser = await puppeteer.launch();
+    try {
+        // Create a browser instance
+        const browser = await puppeteer.launch();
 
-    // Create a new page
-    const page = await browser.newPage();
+        // Create a new page
+        const page = await browser.newPage();
 
-    // Set viewport width and height
-    await page.setViewport({ width: 1600, height: 800 });
+        // Set viewport width and height
+        await page.setViewport({ width: 1600, height: 800 });
 
-    await page.goto(url, { waitUntil: 'networkidle0' });
+        await page.goto(url, { waitUntil: 'networkidle0' });
 
-    // Capture screenshot
-    return page.screenshot({
-        clip: {
-          x: 200,
-          y: 0,
-          width: 1200,
-          height: 800
-        },
-        quality: 100, type: 'jpeg'
-      });
+        // Capture screenshot
+        return page.screenshot({
+            clip: {
+            x: 200,
+            y: 0,
+            width: 1200,
+            height: 800
+            },
+            quality: 100, type: 'jpeg'
+        });
+    }
+    catch (err) {
+        console.log("Encountered an error while trying to screenshot: ", err);
+        return new Uint8Array();
+    }
 }
