fbf81eaede0b1647a0dc05e23021b54a7a10f247
Sawyer Pangborn <sawyer@anesti.org>
2024-12-02 14:36:54 -0700

    Use "venue" for closest landmark

--- src/pelias/pelias.ts
+++ src/pelias/pelias.ts
@@ -4,7 +4,7 @@ import { PELIAS_INSTANCE } from '../constants';
 export async function reverse(lat: number, lon: number, options: Record<string, any> = {}): Promise<any> {
     const requestUrl = new URL("/v1/reverse", PELIAS_INSTANCE);
     
-    Object.entries({ ...options, 'point.lat': lat, 'point.lon': lon }).forEach(([key, value]) => {
+    Object.entries({ ...options, 'point.lat': lat, 'point.lon': lon , "layers": "venue"}).forEach(([key, value]) => {
         requestUrl.searchParams.append(key, String(value));
     });
     console.log(`Querying ${requestUrl}`);
