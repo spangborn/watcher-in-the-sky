3095566cf830f6fc722361cd955b89612bceb9ae
Sawyer Pangborn <sawyer@anesti.org>
2024-12-02 14:06:27 -0700

    more docker progress

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:14 as base
+FROM node:21 as base
 
 WORKDIR /home/node/app
 
--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const TOTAL_CHANGE = parseFloat(process.env.TOTAL_CHANGE || '180');
-export const TIME_WINDOW = parseInt(process.env.TIME_WINDOW || '300000'); // Default 5 minutes
-export const PRUNE_TIME = parseInt(process.env.PRUNE_TIME || '1200000'); // Default 20 minutes
+export const TOTAL_CHANGE = parseFloat(process.env.TOTAL_CHANGE || '1440');
+export const TIME_WINDOW = parseInt(process.env.TIME_WINDOW || '1500000'); // Default 5 minutes
+export const PRUNE_TIME = parseInt(process.env.PRUNE_TIME || '1500000'); // Default 20 minutes
 export const TAR1090_DATA_URL = process.env.TAR1090_DATA_URL || '';
 export const TAR1090_URL = process.env.TAR1090_URL || '';
 export const PELIAS_INSTANCE = process.env.PELIAS_INSTANCE || '';
