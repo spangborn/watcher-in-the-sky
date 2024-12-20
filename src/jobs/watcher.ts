d2b5c4bdd86f20d281bc12a7ea1da30316028056
Sawyer Pangborn <sawyer@anesti.org>
2024-12-19 20:59:37 -0700

    Fix typo

--- src/jobs/watcher.ts
+++ src/jobs/watcher.ts
@@ -13,13 +13,13 @@ export async function detectAircraftFromList(): Promise<void> {
         const { hex, flight, alt_baro, lat, lon } = aircraft;
         if (flight && watchlist.includes(flight.trim()) && alt_baro !== "ground") {
             // We found an aircraft on the watchlist
-            console.log(`Found ${flight} on the watchlist`);
+            console.log(`Found ${flight.trim()} on the watchlist`);
 
             const link = `${TAR1090_URL}?icao=${hex}&zoom=13&lat=${lat.toFixed(4)}&lon=${lon.toFixed(4)}`;
             const screenshotUrl = `${link}&hideButtons&hideSidebar&screenshot`;
 
             const screenshot_data = await captureScreenshot(hex, screenshotUrl);
-            const message = `${'#' + flight.tri()} was detected in the air. \nView more: ${link}`;
+            const message = `${'#' + flight.trim()} was detected in the air. \nView more: ${link}`;
             await postToBluesky(hex, message, screenshot_data);
         }
     }
